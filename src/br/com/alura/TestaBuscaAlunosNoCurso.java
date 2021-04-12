9d08e6ff988fcc03d29e11da2da031d2b861853c
inacio.fabricio.nunes <inacio.fabricio.nunes@gmail.com>
2021-04-12 11:32:28 -0300

    Fim do curso Java Collections: Dominando Listas, Sets e Mapas

---
+++ src/br/com/alura/TestaBuscaAlunosNoCurso.java
@@ -0,0 +1,27 @@
+package br.com.alura;
+
+public class TestaBuscaAlunosNoCurso {
+
+	public static void main(String[] args) {
+		
+		Curso javaColecoes = new Curso("Dominando Java", "José Silva");
+		
+		javaColecoes.adiciona(new Aula("Revisando ArrayList", 25));
+		javaColecoes.adiciona(new Aula("Listas de objetos", 13));
+		javaColecoes.adiciona(new Aula("Relacionamento de lista e objetos", 15));
+		
+		Aluno a1 = new Aluno("José da Silva", 0001);
+		Aluno a2 = new Aluno("Maria da Oliveira", 0002);
+		Aluno a3 = new Aluno("Chico Manoel", 0003);
+		
+		javaColecoes.matricula(a1);
+		javaColecoes.matricula(a2);
+		javaColecoes.matricula(a3);
+		
+		System.out.println("Quem é o aluno com Matricula 0002?");
+		Aluno aluno = javaColecoes.buscaMatriculado(0004);
+		System.out.println(aluno);
+
+	}
+
+}
